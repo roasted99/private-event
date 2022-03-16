617a68a5f202e3884b0d20ed5e3b0f2ddbf3aeba
roasted99 <shwegyimontkyut@gmail.com>
2022-03-16 20:25:42 +0630

    installed application gems and dependencies

--- db/migrate/20220316122324_create_events.rb
+++ db/migrate/20220316122324_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
       t.string :title
-      t.string :detail
+      t.text :detail
       t.datetime :starts_at
       t.string :location
 
