b34a510c6dcc4663851a54d51796793f94a6a693
roasted99 <shwegyimontkyut@gmail.com>
2022-03-17 16:13:20 +0630

    add devise auth and layouts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all
+    @events = Event.all.order("created_at DESC")
   end
 
   # GET /events/1 or /events/1.json
